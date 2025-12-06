f79e3ed4ae73b62de79f732b276a7c50e47b8135
davidocta123 <davi20269ti@student.nurulfikri.ac.id>
2025-12-06 07:37:39 +0700

    belajar tipedata operator

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Tipe Data
+// int nilaiA = 80;
+// bool lulus = false;
+// double nilaiDesimal = 80.8;
+
+// var angka = 10;
+
+// //Output Tipe Data
+// Console.WriteLine("========= TIPE DATA ===========");
+// Console.WriteLine($"Nilai A: {nilaiA}");
+// Console.WriteLine($"Lulus: {lulus}");
+// Console.WriteLine($"Nilai Desimal: {nilaiDesimal}");
+// Console.WriteLine($"Angka (var): {angka}");
+
+// //Operator Aritmatika
+// int a = 60;
+// int b = 10;
+
+// int c = a + b;
+// int d = a - b;
+// int e = a * b;
+// int f = a / b;
+// int g = a % b;
+
+// //Output Operator Aritmatika
+// Console.WriteLine($"{a} + {b} = {c}");
+// Console.WriteLine($"{a} - {b} = {d}");
+// Console.WriteLine($"{a} * {b} = {e}");
+// Console.WriteLine($"{a} / {b} = {f}");
+// Console.WriteLine($"{a} % {b} = {g}");
+
+//Operator Relasi
+// bool r1 = 10 > 5;
+// bool r2 = 10 < 9;
+// bool r4 = 10 >= 5;
+// bool r5 = 10 <= 5;
+// bool r6 = 10 == 5;
+// bool r7 = 10 != 5;
+
+// //Output Operator Relasi
+// Console.WriteLine(r1);
+// Console.WriteLine(r2);
+// Console.WriteLine(r4);
+// Console.WriteLine(r5);
+// Console.WriteLine(r6);
+// Console.WriteLine(r7);
+
+//Operator Logic
+bool a = true && true;
+bool b = true && false;
+bool c = false && true;
+bool d = false && false;
+bool e = true || true;
+bool f = false || false;
+
+
+//Output Operator Logic
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
+Console.WriteLine(d);
+Console.WriteLine(e);
+Console.WriteLine(f);
+
+
+
+
+
+
