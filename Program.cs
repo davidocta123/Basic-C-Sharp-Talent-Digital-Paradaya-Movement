89c5e4a00dbd0566a637615448216884f5bf85a2
davidocta123 <davi20269ti@student.nurulfikri.ac.id>
2025-12-07 05:43:46 +0700

    Belajar Functions

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// Belajar Functions
+class Program
+{
+    static void Main()
+    {
+        int result = Add(7, 9);
+        Console.WriteLine(result);
+    }
+
+    static int Add(int a, int b)
+    {
+        return a + b;
+    }
+}
